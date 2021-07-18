d8d22f6ee287c95219b6618e6d2be2f495c49e5b
Igor <igor.muravyov.pz.2021@ukr.net>
2021-07-18 12:07:34 +0300

    move make logic to build.mk

---
+++ build.mk
@@ -0,0 +1,20 @@
+GHDL?=ghdl
+GHDLFLAGS?=
+TARGET?=target
+
+test: $(MODULES)
+	$(GHDL) -r $(TARGET) --vcd=$(TARGET).vcd
+
+# Binary depends on the object file
+%: %.o
+	$(GHDL) -e $(GHDLFLAGS) $@
+
+# Object file depends on source
+%.o: %.vhd
+	$(GHDL) -a $(GHDLFLAGS) $<
+
+clean:
+	echo "Cleaning up..."
+	rm -f *.o *_testbench $(TARGET) work*.cf e*.lst
+view:
+	gtkwave $(TARGET).vcd
